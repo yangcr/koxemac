517190c170c3215af07bcbc71f7d6189d7be4691
Cristopher Yang <dg.cris.yang@gmail.com>
2022-04-18 22:44:08 -0300

    more fluid pushbar for mobile navbar

--- js/scriptsCatalog.js
+++ js/scriptsCatalog.js
@@ -1,6 +1,30 @@
-initCarouselNoNav();
+const initCarouselNoNav = () => {
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        loop: true,
+        margin: 25,
+        nav: false,
+        dots: true,
+        responsive: {
+            300: {
+                items: 1
+            },
+            700: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            },
+        }
+    });
+}
 
 const pushbar = new Pushbar({
     blur: true,
     overlay: true,
 });
+
+initCarouselNoNav();
