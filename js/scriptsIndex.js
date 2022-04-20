584b94cc95b039f480b622c406db6130de94b289
Cristopher Yang <dg.cris.yang@gmail.com>
2022-04-20 11:47:08 -0300

    minor fixes

--- js/scriptsIndex.js
+++ js/scriptsIndex.js
@@ -1,30 +1,4 @@
-const initCarouselNav= () => {
-        $('.owl-carousel').owlCarousel({
-        center: true,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: true,
-        loop: true,
-        margin: 25,
-        nav: true,
-        dots: true,
-        responsive: {
-            300: {
-                items: 1
-            },
-            700: {
-                items: 2
-            },
-            1000: {
-                items: 4
-            },
-        }
-    });
-    ;
-};
-
 const pushbar = new Pushbar({
     blur: true,
     overlay: true,
 });
-initCarouselNav();
